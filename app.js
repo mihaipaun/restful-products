32d492ab005ca3de0b1f2d8b2865731f80085ed7
Mihai Paun <mihaipaun01@gmail.com>
2012-08-21 17:45:04 +0300

    Setuped a simple model using CRUD methods

--- app.js
+++ app.js
@@ -19,6 +19,94 @@ app.configure(function () {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true}));
 });
 
+// Schema
+var Schema = mongoose.Schema;
+
+// Product model
+var Product = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  style: { type: String, unique: true },
+  modified: { type: Date, default: Date.now }
+});
+
+// use the model
+var ProductModel = mongoose.model('Product', Product);
+
+// Add the CRUD methods
+
+// Read a list of products
+app.get('/api/products', function (req, res) {
+  return ProductModel.find(function (err, products) {
+    if (!err) {
+      return res.send(products);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+// Create a single product
+app.post('/api/products', function (req, res) {
+  var product;
+  console.log("POST: ");
+  console.log(req.body);
+  product = new ProductModel({
+    title: req.body.title,
+    description: req.body.description,
+    style: req.body.style
+  });
+  product.save(function (err) {
+    if(!err) {
+      return console.log("created");
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+// Read a single product by ID
+app.get('/api/products/:id', function (req, res) {
+  return ProductModel.findById(req.params.id, function (err, product) {
+    if (!err) {
+      return res.send(product);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+// Update a single product by ID
+app.put('/api/products/:id', function (req, res) {
+  return ProductModel.findById(req.params.id, function (err, product) {
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.style = req.body.style;
+    return product.save(function (err) {
+      if (!err) {
+        console.log("updated");
+      } else {
+        console.log(err);
+      }
+      return res.send(product);
+    });
+  });
+});
+
+// Delete a single product by ID
+app.delete('/api/products/:id', function (req, res) {
+  return ProductModel.findById(req.params.is, function (err, product) {
+    return product.remove(function (err) {
+      if (!err) {
+        console.log("removed");
+        return res.send('');
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+
 app.get('/api', function (req, res) {
   res.send('eCommerce API is running.');
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 In which I explore creating a RESTful API using Node.js, Express, MongoDB and Mongoose.
 
 ### Notes
+* Read more about [schema types](http://mongoosejs.com/docs/schematypes.html)
+* 
